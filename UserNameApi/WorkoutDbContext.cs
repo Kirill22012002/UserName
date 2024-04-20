e7ed2884c2b103bc43d2ac98d6f3430b3e16a1e8
MightyCall12345 <k.perepechkin>
2024-04-20 08:48:26 +0300

    added docker-compose.yml and ef

---
+++ UserNameApi/WorkoutDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserNameApi.Models.DbModels;
+
+namespace UserNameApi;
+
+public class WorkoutDbContext : DbContext
+{
+    public DbSet<WorkoutExcercise> WorkoutExcercises { get; set; }
+    public DbSet<WorkoutSet> WorkoutSets { get; set; }
+    public DbSet<WorkoutSession> WorkoutSessions { get; set; }
+    public DbSet<Workout> Workouts { get; set; }
+
+    public WorkoutDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
